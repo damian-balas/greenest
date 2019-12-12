2767dbb98c7852490b0507d95835f71a9e0418cf
damian-balas <damian.webdev@gmail.com>
2019-12-12 22:09:21 +0100

    Fetch polluted cities and descriptions, save descriptions to state, run handleSubmit when suggestion is clicked

--- src/App.js
+++ src/App.js
@@ -1,29 +1,87 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './theme/GlobalStyle';
 import theme from './theme/mainTheme';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Form from './components/Form/Form';
+import getDateString from './utils/getDateString';
 
 class App extends Component {
   state = {
     country: '',
     isCountryInvalid: false,
+    cities: [],
+  };
+
+  getPollutedCities = async (countryAbbr, citiesLimit) => {
+    const POLLUTED_CITIES_URL = `https://api.openaq.org/v1/measurements?country=${countryAbbr}&parameter=pm25&sort=desc&limit=500&order_by=value&date_from=${getDateString(
+      new Date(),
+    )}`;
+
+    const response = await axios.get(POLLUTED_CITIES_URL);
+    const { results } = response.data;
+    const citiesSet = new Set();
+
+    results.some(result => {
+      if (!citiesSet.has(result.city)) {
+        citiesSet.add(result.city);
+      }
+      return citiesSet.size === citiesLimit;
+    });
+
+    // remove part after backslash to be able to get data from wikipedia api
+    return [...citiesSet].map(city =>
+      city.includes('/') ? city.substring(0, city.indexOf('/')) : city,
+    );
+  };
+
+  getCitiesDescription = async cities => {
+    const CITIES_DESCRIPTION_URL = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&explaintext&redirects=1&exsentences=2&format=json&origin=*&titles=${cities}`;
+
+    const response = await axios.get(CITIES_DESCRIPTION_URL);
+
+    return response;
   };
 
   handleSubmit = country => {
-    const { countryMap } = this.props;
-    if (!countryMap.has(country)) {
+    const { COUNTRY_MAP, CITIES_LIMIT } = this.props;
+    if (!COUNTRY_MAP.has(country)) {
       this.setState({
         isCountryInvalid: true,
       });
     } else {
-      this.setState({
-        country,
-        isCountryInvalid: false,
-      });
+      this.setState(
+        {
+          country,
+          isCountryInvalid: false,
+        },
+        async () => {
+          try {
+            localStorage.setItem('country', country);
+          } catch {
+            console.warn("Couldn't save to local storage");
+          }
+
+          const countryAbbr = COUNTRY_MAP.get(country);
+          const cities = await this.getPollutedCities(
+            countryAbbr,
+            CITIES_LIMIT,
+          );
+
+          const citiesWikipediaQuery = encodeURI(cities.join('|'));
+          const citiesDescription = await this.getCitiesDescription(
+            citiesWikipediaQuery,
+          );
+          const citiesArray = Object.values(citiesDescription.data.query.pages);
+
+          this.setState({
+            cities: citiesArray,
+          });
+        },
+      );
     }
   };
 
@@ -49,15 +107,17 @@ class App extends Component {
 }
 
 App.defaultProps = {
-  countryMap: new Map([
+  COUNTRY_MAP: new Map([
     ['Poland', 'PL'],
     ['Germany', 'DE'],
     ['Spain', 'ES'],
     ['France', 'FR'],
   ]),
+  CITIES_LIMIT: 10,
 };
 
 App.propTypes = {
-  countryMap: PropTypes.instanceOf(Map),
+  COUNTRY_MAP: PropTypes.instanceOf(Map),
+  CITIES_LIMIT: PropTypes.number,
 };
 export default App;

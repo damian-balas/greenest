a1a38c5d77207109b8259c4e17779fcc3c3bcabb
damian-balas <damian.webdev@gmail.com>
2019-12-14 12:41:57 +0100

    Updates

---
+++ precache-manifest.5a30aa245d748a4b7062292f1da96f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc3b5e583bceed9e87390ed0433de03",
+    "url": "/greenest/index.html"
+  },
+  {
+    "revision": "1a8005412513238b49d2",
+    "url": "/greenest/static/js/2.6ab1b9d9.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/greenest/static/js/2.6ab1b9d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "524d8f89db5375b2298a",
+    "url": "/greenest/static/js/main.9ec2729a.chunk.js"
+  },
+  {
+    "revision": "04e639a272635efa2334",
+    "url": "/greenest/static/js/runtime-main.afaefe05.js"
+  },
+  {
+    "revision": "7d63c75e782f918e1f5328b3a594685a",
+    "url": "/greenest/static/media/environment.7d63c75e.jpg"
+  }
+]);
